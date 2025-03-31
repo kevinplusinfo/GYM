3da5b310413e7881f822a03ad2b13aec209d8eec
kevinplusinfo <kevin.plusinfolab@gmail.com>
2025-03-31 18:23:08 +0530

    some change in admin sidbar

--- resources/views/Admin/layout/sidebar.blade.php
+++ resources/views/Admin/layout/sidebar.blade.php
@@ -1,16 +1,19 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4" >
-    <div class="sidebar">
-        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-            <div class="image">
-                <img src="{{ asset('storage/' . $setting->wlogo) }}" alt="GYM" class="brand-image img-circle elevation-3" alt="User Image" style=""><br>
-				<span class="brand-text font-weight-light" style="color: white">GYM</span>
-            </div>
-            <div class="info text-center">
-                <a href="javascript:void(0);" class="d-block text-center">
+	<a href="{{route('dashbord')}}" class="brand-link">
+		<img src="{{ asset('storage/' . $setting->wlogo) }}" alt="KVN GYM" class="brand-image img-circle elevation-3" style="opacity: .8">
+		<span class="brand-text font-weight-light">KVN GYM</span>
+	</a>
+	<div class="sidebar">
+		<div class="user-panel mt-3 pb-3 mb-3 d-flex">
+			<div class="image">
+				<img src="{{ asset('storage/' . (Auth::user()->profile_image ?? 'default-avatar.jpg')) }}" class="img-circle elevation-3" alt="User Image" style="">
+			</div>
+			<div class="info text-center">
+				<a href="javascript:void(0);" class="d-block text-center">
+					{{ Auth::user()->name }}
 				</a>
             </div>
         </div>
-
         <div class="form-inline">
             <div class="input-group" data-widget="sidebar-search">
                 <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
@@ -97,7 +100,7 @@
 				  </li>
 				  <li class="nav-item">
 					<a href="{{route('purchaseplan.index')}}" class="nav-link  purchaseplan-link">
-						<i class="fa-solid fa-users nav-icon"></i>
+						<i class="fa-solid fa-credit-card nav-icon"></i>
 						<p>
 							Purchase Plan
 						</p>
